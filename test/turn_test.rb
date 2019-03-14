9cb97190b780b6b36fc7c2392c829dc15586b6bc
Kylie Stanley <knstanley11@gmail.com>
2019-03-14 16:26:33 -0600

    Refactor Turn tests

--- test/turn_test.rb
+++ test/turn_test.rb
@@ -4,39 +4,30 @@ require './lib/turn'
 require './lib/card'
 
 class TurnTest < Minitest::Test 
+  attr_reader :turn, :card
+
+  def setup
+    @card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    @turn = Turn.new('Juneau', card)
+  end
 
   def test_it_exists
-    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    turn = Turn.new('Juneau', card)
-
     assert_instance_of Turn, turn
   end
 
   def test_it_has_a_card
-    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    turn = Turn.new('Juneau', card)
-
     assert_equal card, turn.card
   end
 
   def test_it_has_a_guess
-    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    turn = Turn.new('Juneau', card)
-
     assert_equal 'Juneau', turn.guess
   end
 
   def test_it_returns_true_if_guess_is_correct
-    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    turn = Turn.new('Juneau', card)
-
     assert_equal true, turn.correct?
   end
 
   def test_it_returns_correct_feedback
-    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    turn = Turn.new('Juneau', card)
-
     assert_equal 'Correct!', turn.feedback
   end
 
