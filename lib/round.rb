77ffc0e06f76ccce6c5ee7dc500b57b456776217
Kylie Stanley <knstanley11@gmail.com>
2019-03-15 13:20:52 -0600

    Add methods to Round class

---
+++ lib/round.rb
@@ -0,0 +1,29 @@
+class Round 
+  attr_reader :deck
+
+  def initialize(deck)
+    @deck = deck
+  end
+
+  def turns
+    
+  end
+
+  def current_card
+  end
+
+  def take_turn(guess)
+  end
+
+  def number_correct
+  end
+
+  def number_correct_by_category(category)
+  end
+
+  def percent_correct
+  end
+
+  def percent_correct_by_category(category)
+  end
+end
